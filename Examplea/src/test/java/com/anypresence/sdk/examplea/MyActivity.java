8ba8cefb88e71930205cecbbb424daccdb2fd14f
Android Generator <android_generator@anypresence.com>
2014-12-18 16:46:35 +0000

    Purging existing sources.

--- Examplea/src/test/java/com/anypresence/sdk/examplea/MyActivity.java
+++
@@ -1,9 +0,0 @@
-package com.anypresence.sdk.examplea;
-
-import android.app.Activity;
-import android.os.Bundle;
-
-public class MyActivity extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-	}
-}
